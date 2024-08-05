9697dec72cc9f442e7c605a3e293d0e9f588d8a8
Harsh Zala <140790325+harshzala1@users.noreply.github.com>
2024-08-05 11:11:42 +0530

    Add files via upload

---
+++ Data-structure/lab-19/InsertionSort1.java
@@ -0,0 +1,34 @@
+public class InsertionSort1 {
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+}
